dd05f9ddad51ced68a668469f84532161a67737f
Aidan Tilgner <aidantilgner02@gmail.com>
2022-12-18 16:19:13 -0800

    getting this working

--- src/lib/server/bootstrap/axios.server.ts
+++
@@ -1,5 +0,0 @@
-import axios from "axios";
-
-const OnyxCoreInstance = axios.create({
-    baseURL: "https://api.onyxcore.io",
-})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script lang="ts">
+	import { checkAuth } from '$lib/helpers/auth';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const auth = await checkAuth();
+		if (!auth) {
+			window.location.href = '/login';
+		}
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
